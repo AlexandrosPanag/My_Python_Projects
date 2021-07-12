658a8c52b7cad90546e7e0d3c324f45cbf7b80b4
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-07-12 06:01:45 +0300

    Update String_Input.py

--- String_Input.py
+++ String_Input.py
@@ -1,2 +1,2 @@
-name= input("Enter username: ")
-print("Player has joined, "+name)
+name= input("Enter username: ") #we print a text & receive user input and then store into the name variable
+print("Player has joined, "+name) #we print a text along with the received user input and then we print the variable
