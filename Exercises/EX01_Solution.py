6e62ed395c1c7412fa34af51a7234878979250ae
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-07-12 06:18:04 +0300

    Add files via upload

---
+++ Exercises/EX01_Solution.py
@@ -0,0 +1,15 @@
+#the formula is 800*12 - 43%
+paycheck =0
+monthly_paycheck=800
+tax=43/100
+
+paycheck=12*monthly_paycheck #we calculate the year paycheck
+print("The payment per year is: ")
+print(paycheck)
+
+paycheck=paycheck-paycheck*tax #we calculate the year paycheck
+print("The payment after tax is: ")
+print(paycheck)
+
+
+
