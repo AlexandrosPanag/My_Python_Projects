6a658b026fe0a9f8f5f5b41be04932848ee27ca7
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-07-12 05:28:04 +0300

    Update Arithmetic_Operations_In_Python.py

--- Arithmetic_Operations_In_Python.py
+++ Arithmetic_Operations_In_Python.py
@@ -15,3 +15,4 @@ print(2+2+4 - 2/3) #we do result_a=2/3 then result_b=2+2+4 then result_b-result_
 print((y**2)**0.5) #we do (y^2) ^0.5 basically 2^2=4 and then 4^0.5 the result will be 2.0
 print(z ** 2 + z/2*3) #we do z^2 (3^2=9) , then z/2=1.5 ,then 1.5*3 =4.5 then we finally add 9+4.5 the result will be 13.5 
 print(o/10) #we do 200/10
+print(3%2) #we do 3 modulo 2 the result will be 1
