0099a913d0f1a74af5c6f844f7ce0c8293a1f7e1
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-07-23 22:15:01 +0300

    Add files via upload

---
+++ Strings_In_Python/Strings_Basics.py
@@ -0,0 +1,8 @@
+name="Alex"
+#>>>name[0]  will return A
+#>>>type(name) will return <class 'str'
+#>>>ord("A") will return the unicode format of the character
+s="Hello from the other side"
+print("Hello , \n ", s) #will return the from the other side string on a new line
+#>>> s[0:4] it will print Hello -- Python Slice Function
+#>>> len(s) it will return the length of the string
