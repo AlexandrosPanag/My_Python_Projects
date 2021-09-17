d2ec1b193c4eedc8af85f9ee834fc49073bf1794
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-09-17 11:26:14 +0300

    Add files via upload

---
+++ Boolean Operator_AND_OR_NOT.py
@@ -0,0 +1,7 @@
+if( 1 == 1) and (2+1 > 3):
+    print("The first statement is true/n") #false statement because 1=1 is true & 2+1 > 3 is false == true + false == false
+elif (1 == 1 or 2 == 1): 
+    print("The second statement is true") #True statement because 1 == 1 is true 2 == 1 is false == TRUE OR FALSE = True
+if(not 1 > 9):
+    print("The third statement is true")
+
