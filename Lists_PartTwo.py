59cc0207901cca7fb9986d0442d1aecc1e57b084
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-01 10:37:29 +0300

    Add files via upload

---
+++ Lists_PartTwo.py
@@ -0,0 +1,4 @@
+mylist=[1,4,6,3,2,0] #creating a list with random elements
+mylist.sort() #sorting the elements from the list in order from 0 to 9 etc
+mylist.reverse() #reversing the sorted element from 9 to 0 etc
+print(mylist)
