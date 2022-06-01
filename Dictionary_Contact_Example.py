4a97e857cafc9ffa5f8ac883d5b13b75821b4b16
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-01 15:02:24 +0300

    Update Dictionary_Contact_Example.py

--- Dictionary_Contact_Example.py
+++ Dictionary_Contact_Example.py
@@ -12,4 +12,4 @@ print("Your contacts are:\n",contact)
 #sorting the contacts in alphabetical order
 contact_sorted=list(contact.items()) #transfering the contacts into a new dictionary
 contact_sorted.sort() #sorting the contact list
-print("The contact list has been sorted:\n",contact_sorted)
+print("The contact list has been sorted:\n",contact_sorted) #print the sorted contact list
