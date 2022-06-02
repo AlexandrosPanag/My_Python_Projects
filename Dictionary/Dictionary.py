b74e8925d4967e553ac5cf91259ae24e3c457a53
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 11:10:46 +0300

    Add files via upload

---
+++ Dictionary/Dictionary.py
@@ -0,0 +1,5 @@
+#Contacts usually have the format "PrimaryKeyElement":<keyvalue>, but we cannot alter the primary key element
+contacts = {"John":999999999, "Doe":888888888} #declaring a dictionary
+contacts["Doe"] = 777777777 #altering a value that belongs to Doe
+print(len(contacts))
+print(contacts)
