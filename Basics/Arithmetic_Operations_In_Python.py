0ffbeaef44fe147e7f242af28df15152011c610c
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 11:20:30 +0300

    Add files via upload

---
+++ Basics/Arithmetic_Operations_In_Python.py
@@ -0,0 +1,18 @@
+a=2.0 #we declare a equal to 2
+b=3.0 #we declare b equal to 3
+c=4.5 #we declare c equal to 4.5
+
+y = 2 #we declare y equal to 2
+z= 3 #we declare z equal to 3
+o = 2E2 #we declare the value o=2 *100 =200.0
+
+
+#basic arithmetic operations in python
+
+print(a + b) #we add a+b
+print((a + b) * 2) #we do (a+b) and then we multiply it by 2
+print(2+2+4 - 2/3) #we do result_a=2/3 then result_b=2+2+4 then result_b-result_a 
+print((y**2)**0.5) #we do (y^2) ^0.5 basically 2^2=4 and then 4^0.5 the result will be 2.0
+print(z ** 2 + z/2*3) #we do z^2 (3^2=9) , then z/2=1.5 ,then 1.5*3 =4.5 then we finally add 9+4.5 the result will be 13.5 
+print(o/10) #we do 200/10
+print(3%2) #we do 3 modulo 2 the result will be 1
