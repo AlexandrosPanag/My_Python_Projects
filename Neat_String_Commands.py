40103f032c39682dd255c0cbd0e6897d167e0505
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 11:31:31 +0300

    Delete Neat_String_Commands.py

--- Neat_String_Commands.py
+++
@@ -1,6 +0,0 @@
-s = 'Word'
-print(len(s.strip())) # this line outputs the size of the string
-
-print(s.upper()) # this line outputs the word in capital form
-
-print(len('This is a string text'.split())) #it will output the words seperately
