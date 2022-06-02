df09ded34cdc664869f9b3320dcfb641f5020c78
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 11:19:01 +0300

    Delete String_Input.py

--- String_Input.py
+++
@@ -1,2 +0,0 @@
-name= input("Enter username: ") #we print a text & receive user input and then store into the name variable
-print("Player has joined, "+name) #we print a text along with the received user input and then we print the variable
