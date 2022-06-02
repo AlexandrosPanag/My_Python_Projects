d826e6c955eb6cea3d2df4d975128b6ff2a10ec5
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 14:55:56 +0300

    Add files via upload

---
+++ Basics/While_Multiple_Input_Choice.py
@@ -0,0 +1,11 @@
+while True:
+    aw=input("Choose one of the following :\n 1 for A,\n 2 for B,\n 3 for Exit: ")
+    if aw=="1":
+        print("\n You have chosen A")
+    elif aw=="2":
+        print("\n You have chosen B")
+    elif aw=="3":
+        break
+    else:
+        continue
+print("End of the program!")
