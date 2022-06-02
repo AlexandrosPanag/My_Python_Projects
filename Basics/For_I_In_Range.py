d4e2e1ce1a18ad1f80f0d10c83d2aa84decb5ea5
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 11:58:03 +0300

    Add files via upload

---
+++ Basics/For_I_In_Range.py
@@ -0,0 +1,13 @@
+#for in in range (start, end , step)
+
+for i in range (1,10,2):
+    print(i,end=",") #this will print from 1 to 10 with +2 step 
+
+
+print("\n")
+for i in range (1,10):
+    print(i,end=",") #this will print from 1 to 10 with +1 default step
+
+print("\n")
+for i in range(10):
+    print(i,end=",") #this will print from 0 to 9 with +1 step 
