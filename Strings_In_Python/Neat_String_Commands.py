48323d30669e60a06fa965ba1adad23db17787d9
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 11:30:49 +0300

    Add files via upload

---
+++ Strings_In_Python/Neat_String_Commands.py
@@ -0,0 +1,6 @@
+s = 'Word'
+print(len(s.strip())) # this line outputs the size of the string
+
+print(s.upper()) # this line outputs the word in capital form
+
+print(len('This is a string text'.split())) #it will output the words seperately
