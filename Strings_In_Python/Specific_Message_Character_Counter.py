e3ab90322bd4a6aa101057036566629cf96cc0b7
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 12:48:14 +0300

    Update Specific_Message_Character_Counter.py

--- Strings_In_Python/Specific_Message_Character_Counter.py
+++ Strings_In_Python/Specific_Message_Character_Counter.py
@@ -3,6 +3,7 @@ letter_counter = {} #creating a counter to count each letter
 text = input("Give a message to count the length :")
 
 for i in text: 
+    #if i.isalpha(): optional command if we want to skip the null to be counted
     letter_counter[i] = letter_counter.get(i,0) + 1
 
 for i in sorted(letter_counter.keys()):
