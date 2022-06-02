8e0a812eed68c518cf6c7333b07a1a241fa91b16
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 11:34:48 +0300

    Delete Lists_PartTwo.py

--- Lists_PartTwo.py
+++
@@ -1,4 +0,0 @@
-mylist=[1,4,6,3,2,0] #creating a list with random elements
-mylist.sort() #sorting the elements from the list in order from 0 to 9 etc
-mylist.reverse() #reversing the sorted element from 9 to 0 etc
-print(mylist)
