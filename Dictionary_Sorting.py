4fa2c3f9af494c6a2f3a22cf06f6ac9382cf8f35
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 10:39:35 +0300

    Add files via upload

---
+++ Dictionary_Sorting.py
@@ -0,0 +1,17 @@
+dd = {1:20, 5:10, 3:15}
+
+print("\nOnly the dictionary keys are printed: \n")
+for i in dd: #printing only the keys of the dictionary
+    print(i)
+
+print("\nThe dictionary keys and the elements are printed: \n")
+for i in dd:
+    print(i,'->',dd[i]) #printing the key and where the dictionary is showing
+
+print("\nSorted order by dictionary key order: \n")
+for i in sorted(dd): 
+    print(i,'->',dd[i]) #printing the key sorted by the key value in order
+
+print("\nSorted order by dictionary element order: \n")
+for i in sorted(dd,key = dd.get):
+    print(i,'->',dd[i])
