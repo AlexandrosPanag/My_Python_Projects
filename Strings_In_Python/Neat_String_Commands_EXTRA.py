ee9a9cc54ae3d802cbb61553dacea62baea23fbe
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 11:32:35 +0300

    Add files via upload

---
+++ Strings_In_Python/Neat_String_Commands_EXTRA.py
@@ -0,0 +1,12 @@
+
+print(10,20,30, sep="\t") # it will output 10 <tab> 20 <tab> 30 <tab>
+
+print(10,20,30, sep="&") # it will output 10&20&30
+
+print(10,20,30, sep="\n") # it will output 10 \n 20 \n 30 \n
+
+print("The temperature in %s is : %s" %("Celsius",20)) # it will print out in a line the words seperetely
+
+piee= 3.141592653589793238 #declaring the full Pi number 
+
+print("The value of Pie is %1.2f"%(piee)) # it will output Pi as 3.14 instead of its full variant
