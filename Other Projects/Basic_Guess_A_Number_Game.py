ebd356b1b36a33c1964fb2dfafa0f0e963691350
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-02 11:02:18 +0300

    Add files via upload

---
+++ Other Projects/Basic_Guess_A_Number_Game.py
@@ -0,0 +1,8 @@
+list = [1,33,67,89,24,12,124,93]
+userinput = int(input("Guess a number from the list: "))
+for i in list:
+    if i == userinput:
+        print("You guessed correctly, you found a number!")
+        break
+    else:
+        print("Wrong guess!")
