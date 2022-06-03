a7ef9de7c153382c5efffed09921efcbb5c0df05
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-03 12:57:32 +0300

    Update List_Comprehension.py

--- Lists/List_Comprehension.py
+++ Lists/List_Comprehension.py
@@ -3,4 +3,4 @@ for i in range(1,30): #from the numbers 1-30
     if i%2==0 and i %3 == 0:
         mylist.append(i)
 
-print(mylist)  #print the numbers from [1 to 30] that can be multiplied by 2 and 3 at the same time
+print(mylist)  #print the numbers from [1 to 30] that can be divided by 2 and 3 at the same time
