c96948651e3e38effb6671bd6be22b9527236c03
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-03 12:58:41 +0300

    Delete List_Comprehension.py

--- Lists/List_Comprehension.py
+++
@@ -1,6 +0,0 @@
-mylist = [] #creating a null list
-for i in range(1,30): #from the numbers 1-30
-    if i%2==0 and i %3 == 0:
-        mylist.append(i)
-
-print(mylist)  #print the numbers from [1 to 30] that can be divided by 2 and 3 at the same time
