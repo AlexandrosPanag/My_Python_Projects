5e27e7d0b8bbd439db78fb1667662373c6932412
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-09 14:53:02 +0300

    Add files via upload

---
+++ Lists/Lists_Slice_III.py
@@ -0,0 +1,2 @@
+numbers_mylist = [2,3,5,6,7,2,1,2,4,5,6,7,8,8,9,2,1,3,3,4,5,5,5]
+print(numbers_mylist[6:]) #if we dont declare the second <initiator> of the slice command then python will slice the first 6 elements basically we do slice[6:end_of_list]
