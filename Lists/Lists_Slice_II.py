53545ed1ace427e6d7624cdbfd8ade7742339b02
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-09 14:50:31 +0300

    Add files via upload

---
+++ Lists/Lists_Slice_II.py
@@ -0,0 +1,2 @@
+numbers_mylist = [2,3,5,6,7,2,1,2,4,5,6,7,8,8,9,2,1,3,3,4,5,5,5]
+print(numbers_mylist[:6]) #if we dont declare the first <initiator> then the list will slice from the starting element (its like [0:6])
