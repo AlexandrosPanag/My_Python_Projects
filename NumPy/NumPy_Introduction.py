d91242c0bba2025e18da7b40e2ac6df670c26a2a
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-10 10:46:00 +0300

    Add files via upload

---
+++ NumPy/NumPy_Introduction.py
@@ -0,0 +1,9 @@
+import numpy as np #importing numpy you might need to run the command <pip install numpy>, in case it is not
+#recognized by visual studio code , set the numpy name to np
+
+numbers = np.array([2,3,5,6,11]) #create a numpy array
+
+print(type(numbers)) #notice how the type is printed as class named numpy.ndarray
+print(np.ndarray) 
+
+print(numbers) #however the array is printed normally
