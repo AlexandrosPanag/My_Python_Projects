3fcaf3b63db76795ea300d01a47ef374a559cfe7
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-10 12:56:35 +0300

    Add files via upload

---
+++ NumPy/NumPy_Array_Arange.py
@@ -0,0 +1,11 @@
+import numpy as np #importing numpy you might need to run the command <pip install numpy>, in case it is not
+#recognized by visual studio code , set the numpy name to np
+
+Myarray = np.arange(9) #creates an array with 0-9 elements
+print("\nMy Array: \n",Myarray)
+
+Mysecondarray = np.arange(5,10) #creates a second array from 5 to (10-1) elements
+print("\nMy Second array:\n",Mysecondarray)
+
+Mythirdarray = np.arange(10,1,-2) #creates a third array from 10 to 1 with -2 step elements
+print("\nMy Third array:\n",Mythirdarray) 
