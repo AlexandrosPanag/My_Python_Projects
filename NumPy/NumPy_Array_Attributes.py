f69d34f057cfa8d8deb15139e4d7741176774539
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-10 11:17:44 +0300

    Update NumPy_Array_Attributes.py

--- NumPy/NumPy_Array_Attributes.py
+++ NumPy/NumPy_Array_Attributes.py
@@ -1,9 +1,9 @@
 import numpy as np #importing numpy you might need to run the command <pip install numpy>, in case it is not
 #recognized by visual studio code , set the numpy name to np
 
-integers = np.array([[1, 2, 3],[4 ,5 ,6]]) #in some platforms it will output int64, the output in this case was int32
+integers = np.array([[1, 2, 3],[4 ,5 ,6]]) #declaring an integer array
  
-floats = np.array([0.0, 0.1, 0.2, 0.3, 0.4]) #the output will be float 64
+floats = np.array([0.0, 0.1, 0.2, 0.3, 0.4]) #declaring a float array
 
-print(integers.dtype)
-print(floats.dtype)
+print(integers.dtype) #in some platforms it will output int64, the output in this case was int32
+print(floats.dtype) #the output will be float 64
