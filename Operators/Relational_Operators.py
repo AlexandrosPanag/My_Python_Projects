a9924723451883129aeca699ad4c1ebc08d3668a
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-01-02 12:19:25 +0200

    Add files via upload

---
+++ Operators/Relational_Operators.py
@@ -0,0 +1,11 @@
+#Relational Operators in Python
+
+a = 10
+b = 11
+
+a == b #if a is equal to b
+a != b #if a is not equal to b
+a > b #if a is greater than b
+a >= b #if a is greater than or equal to b
+a < b #if a is less than b
+a <= b #if a is less than or equal than b 
