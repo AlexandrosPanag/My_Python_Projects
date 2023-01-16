52d59b3b55631fd1f0372f30a5a4ac999ce82b94
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-01-17 01:58:00 +0200

    Delete Arrays directory

--- Arrays/For_2Dimensional_Array.py
+++
@@ -1,9 +0,0 @@
-# By Alexandros Panagiotakopoulos - alexandrospanag.github.io
-
-myarray=[[1,2,3,4],[5,5,6,7],[3,4,5,8]] #creating the array
-
-
-for i in myarray: #for the first line
-    for y in i: #for the first row
-        print(y,end="\t")
-    print()
