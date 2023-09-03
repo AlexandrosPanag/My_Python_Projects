4f495223e3da024bfc2551e52690844bede1b718
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-03 22:19:54 +0300

    Add files via upload

---
+++ Basics/Basic_User_Input_Calculator.py
@@ -0,0 +1,10 @@
+# By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+x = input("Give a number")
+y = input("Give a second number")
+
+print(x+y)
+print(x-y)
+print(x/y)
+print(x*y)
+print(x%y)
