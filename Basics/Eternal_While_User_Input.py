3e1e29a5a5b0589fbf8370255f9427e70ec5de9c
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2025-05-05 12:37:08 +0300

    Delete Basics/Eternal_While_User_Input.py

--- Basics/Eternal_While_User_Input.py
+++
@@ -1,6 +0,0 @@
-# By Alexandros Panagiotakopoulos - alexandrospanag.github.io
-
-while True:
-    n = input("Give a name or type 0 for exit:")
-    if n =='0':break
-    else:print("You have entered:",n.upper())
