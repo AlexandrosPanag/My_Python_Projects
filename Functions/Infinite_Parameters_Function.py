e268b0df2c5f160b7418bb595f44cb9a32ea5380
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2025-05-05 12:20:45 +0300

    Delete Functions directory

--- Functions/Infinite_Parameters_Function.py
+++
@@ -1,8 +0,0 @@
-# By Alexandros Panagiotakopoulos - alexandrospanag.github.io
-
-# *is a container of single items - **is a a keyword of pair
-def func(*args, **keywords):
-	for a in args:print(a)
-	for k,v in keywords.items():print(k,v)
-
-func(1,2,3, x=10, y=20)
