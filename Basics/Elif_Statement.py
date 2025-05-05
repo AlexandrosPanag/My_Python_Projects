40be11ef1806a55113e4fb8434d82d95de9649c8
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2025-05-05 12:38:48 +0300

    Delete Basics/Elif_Statement.py

--- Basics/Elif_Statement.py
+++
@@ -1,9 +0,0 @@
-# By Alexandros Panagiotakopoulos - alexandrospanag.github.io
-
-num=5
-if num == 1:
-    print("One")
-elif num == 2:
-        print("Two")
-elif num > 2:
-        print("The number is bigger than 2")
